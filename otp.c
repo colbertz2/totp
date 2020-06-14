ed52a764f6da88a1f9e15598332e71a8aba09f48
Zach Colbert <colbertz@oregonstate.edu>
2020-06-13 17:18:39 -0700

    Progress! Another submission

--- otp.c
+++ otp.c
@@ -21,33 +21,54 @@
 #define KEY_LENGTH 32
 
 int get_otp() {
-    unsigned char *K, *T;
-    unsigned int digest_len;
-    unsigned char digest[EVP_MAX_MD_SIZE];
+    int i;
+    unsigned long totp;
+    unsigned char *digest;
+    unsigned char trunc[9];
+    memset(trunc, '\0', 9 * sizeof(char));
 
-    // Read key from keyfile
-    K = _otp_read_key();
+    // unsigned char* K = _otp_read_key();
+    unsigned char* K = _otp_read_key();
+    printf("K = %s\n", K);
 
-    // Get time value T
-    T = _otp_get_time();
+    // unsigned char* T = _otp_get_time();
+    unsigned char* T = _otp_get_time();
+    printf("T = %s\n", T);
 
-    // Can't get HMAC working before submission deadline
-    fprintf(stderr, "DEBUG: K=%s\n", K);
-    fprintf(stderr, "DEBUG: T=%s\n", T);
+    // HMAC seems to be returning the same digest
+    // for different messages (T) with the same key
+    digest = HMAC(EVP_sha1(),
+                K, sizeof(K) - 1,
+                T, sizeof(T) - 1,
+                NULL, NULL);
 
-    /***
-    // Run HMAC
-    HMAC(EVP_sha1(),
-         K, strlen(K),
-         T, strlen(T),
-         digest, &digest_len);
-    ***/
+    printf("HMAC(K, T) = ");
+    for (i = 0; i < strlen(digest); i++) {
+        printf("%x ", (int) digest[i]);
+    }
+    printf("\n");
 
     // Truncate HMAC result
-    // printf("%s\n", digest);
+    i = digest[19] & 0xf;
+
+    // Copy byte values (hex) into a string literal
+    sprintf(trunc, "%x%x%x%x",
+            (int)(digest[i]   & 0x7f),
+            (int)(digest[i+1] & 0xff),
+            (int)(digest[i+2] & 0xff),
+            (int)(digest[i+3] & 0xff));
+    printf("TRUNC = %s\n", trunc);
+
+    // Cast to unsigned long and take mod 10^6
+    totp = strtoul(trunc, NULL, 16);
+    printf("TRUNC (ulong) = %lu\n", totp);
+
+    totp = totp % 1000000;
+    printf("TOTP = %lu\n", totp);
 
     free(K);
     free(T);
+    // free(digest);
     return 0;
 }
 
--- test_otp.c
+++ test_otp.c
@@ -2,19 +2,69 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+#include <openssl/hmac.h>
+#include <openssl/evp.h>
 
 #include "otp.h"
 
 int main() {
-    char* key = _otp_read_key();
-    printf("%s\n", key);
+    int i;
+    unsigned long totp;
+    unsigned char *digest;
+    unsigned char trunc[9];
+    memset(trunc, '\0', 9 * sizeof(char));
 
-    char* T = _otp_get_time();
-    printf("%s\n", T);
+    // unsigned char* K = _otp_read_key();
+    unsigned char* K = _otp_read_key();
+    printf("K = %s\n", K);
 
-    get_otp();
+    // unsigned char* T = _otp_get_time();
+    unsigned char* T = _otp_get_time();
+    printf("T = %s\n", T);
 
-    free(key);
+    digest = HMAC(EVP_sha1(),
+                K, sizeof(K) - 1,
+                T, sizeof(T) - 1,
+                NULL, NULL);
+
+    printf("HMAC(K, T) = ");
+    for (i = 0; i < strlen(digest); i++) {
+        printf("%x ", (int) digest[i]);
+    }
+    printf("\n");
+
+    // Truncate HMAC result
+    i = digest[19] & 0xf;
+
+    sprintf(trunc, "%x%x%x%x",
+            (int)(digest[i]   & 0x7f),
+            (int)(digest[i+1] & 0xff),
+            (int)(digest[i+2] & 0xff),
+            (int)(digest[i+3] & 0xff));
+    printf("TRUNC = %s\n", trunc);
+
+    // memset(trunc, digest[i] & 0x7f, 1);
+    // memset(trunc + 1, digest[i+1] & 0xff, 1);
+    // memset(trunc + 2, digest[i+2] & 0xff, 1);
+    // memset(trunc + 3, digest[i+3] & 0xff, 1);
+    // printf("TRUNC = ");
+    // for (i = 0; i < strlen(trunc); i++) {
+    //     printf("%x ", (int) trunc[i]);
+    // }
+    // printf("\n");
+
+    // Cast to unsigned int and take mod 10^6
+    // totp = (unsigned int) trunc % 1000000;
+    totp = strtoul(trunc, NULL, 16);
+    printf("TRUNC (ulong) = %lu\n", totp);
+
+    totp = totp % 1000000;
+    printf("TOTP = %lu\n", totp);
+
+    free(K);
+    free(T);
+    // free(digest);
     return 0;
 }
