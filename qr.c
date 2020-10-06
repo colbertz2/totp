9467af3d13fc90a56fe019784c6ba5a8e371e225
Zach Colbert <zach.colbert20@gmail.com>
2020-10-05 17:26:57 -0700

    Implement QR code generation with GD Image and qrencode lib

--- qr.c
+++ qr.c
@@ -1,141 +1,187 @@
 /*******************************************************
  * qr.c - Shared secret and qr code generation methods
- * 
+ *
  * AUTHOR
  *      Zach Colbert <colbertz@oregonstate.edu>
  *******************************************************/
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include "qr.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <gd.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <pwd.h>
+#include <qrencode.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <fcntl.h>
-#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <errno.h>
-#include <pwd.h>
-#include <openssl/rand.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
-
-#include "qr.h"
-#include "qrgen/qrcodegen.h"       // Thanks to @nayuki for the library!
 
 #define KEY_LENGTH 32
 
 // "Private" methods
-char* _qr_keygen();
-int _qr_img(const char* uri);
+char *_qr_keygen();
+gdImagePtr _qr_img(QRcode *, int[3], int[3], int, int);
 
 // Generate key for TOTP, compile info for GA URI,
 // and encode this URI in a QR code
 int qr_generate() {
-    char *key, *uname, *keyfname = "secret";
-    char *hostname = calloc(101, sizeof(char));
-    char *uri = calloc(275, sizeof(char));
-    int status, keyfd;
-    struct passwd *pw;
-    
-    // Generate key and write to file
-    /************************************************
-     * NOTE: This app overwrites the key file each
-     * time a new one is generated. We only support
-     * one user at a time.
-     ************************************************/
-    key = _qr_keygen();
-    do {
-        keyfd = open(keyfname, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    } while (keyfd == -1);
-    write(keyfd, key, strlen(key) * sizeof(char));
-    close(keyfd);
-    // fprintf(stderr, "DEBUG: secret %s\n", key); // @DEV
+  char *key, *uname, *keyfname = "secret";
+  char *hostname = calloc(101, sizeof(char));
+  char *uri = calloc(275, sizeof(char));
+  int status, keyfd;
+  struct passwd *pw;
 
-    // Get user id
-    pw = getpwuid(getuid());
-    uname = pw->pw_name;
+  int QRversion = 0;                 // Allow library to choose code size
+  QRecLevel QRlevel = QR_ECLEVEL_M;  // Error correction level
+  QRencodeMode QRmode = QR_MODE_8;   // UTF-8 mode
+  int QRcase = 1;                    // Case-sensitive data
+  QRcode *qrData;
 
-    // Get hostname
+  int int_bg_color[3] = {255, 255, 255};  // QR Code image background color
+  int int_fg_color[3] = {0, 0, 0};        // QR Code image foreground color
+  int img_size = 1000;                    // Image size
+  int img_margin = 25;                    // Image margin
+  int img_quality = 100;                  // JPEG quality (for GD)
+  char *imgfname = "scanme.jpg";          // Filename for image output
+  FILE *imgfile;                          // File descriptor for image out
+  gdImagePtr qrImg;
+
+  // Generate key and write to file
+  /************************************************
+   * NOTE: This app overwrites the key file each
+   * time a new one is generated. We only support
+   * one user at a time.
+   ************************************************/
+  key = _qr_keygen();
+  do {
+    keyfd = open(keyfname, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+  } while (keyfd == -1);
+  write(keyfd, key, strlen(key) * sizeof(char));
+  close(keyfd);
+  // fprintf(stderr, "DEBUG: secret %s\n", key); // @DEV
+
+  // Get user id
+  pw = getpwuid(getuid());
+  uname = pw->pw_name;
+
+  // Get hostname
+  memset(hostname, '\0', 101 * sizeof(char));
+  status = gethostname(hostname, 100);
+  if (status == -1) {
     memset(hostname, '\0', 101 * sizeof(char));
-    status = gethostname(hostname, 100);
-    if (status == -1) {
-        memset(hostname, '\0', 101 * sizeof(char));
-        sprintf(hostname, "localhost");
-    }
+    sprintf(hostname, "localhost");
+  }
 
-    // Compile uri
-    memset(uri, '\0', 275 * sizeof(char));
-    sprintf(uri, "otpauth://totp/CS370:%s@%s?secret=%s&issuer=CS370",
-            uname, hostname, key);
-    // fprintf(stderr, "DEBUG: uri %s\n", uri);    // @DEV
+  // Compile uri
+  memset(uri, '\0', 275 * sizeof(char));
+  sprintf(uri, "otpauth://totp/CS370:%s@%s?secret=%s&issuer=CS370", uname,
+          hostname, key);
+  // fprintf(stderr, "DEBUG: uri %s\n", uri);    // @DEV
 
-    // Generate QR code
-    status = _qr_img(uri);
+  // Encode uri into QR Code
+  qrData = QRcode_encodeString(uri, QRversion, QRlevel, QRmode, QRcase);
+  if (qrData == NULL) {
+    fprintf(stderr, "Error encoding uri to bytes: %s\n", strerror(errno));
+    return 1;
+  }
 
-    // Free buffers
-    free(key);
-    free(hostname);
-    free(uri);
+  // Open file for QR image
+  imgfile = fopen(imgfname, "w");
+  if (imgfile == NULL) {
+    fprintf(stderr, "Unable to open output file: %s\n", imgfname);
+    return 1;
+  }
 
-    return 0;
+  // Encode qr data as gd image
+  qrImg = _qr_img(qrData, int_fg_color, int_bg_color, img_size, img_margin);
+
+  // Write to file and close
+  gdImageJpeg(qrImg, imgfile, img_quality);
+
+  // Free buffers
+  free(key);
+  free(hostname);
+  free(uri);
+  QRcode_free(qrData);
+  gdImageDestroy(qrImg);
+  fclose(imgfile);
+
+  printf("TOTP barcode saved to: %s\n", imgfname);
+
+  return 0;
 }
 
 // Generate key for TOTP, convert to base32, and save to file
-char* _qr_keygen() {
-    int i, status = 0;
-    unsigned char *buffer = calloc(KEY_LENGTH + 1, sizeof(char));
-    char *b32 = calloc(KEY_LENGTH + 1, sizeof(char));
+char *_qr_keygen() {
+  int i, status = 0;
+  unsigned char *buffer = calloc(KEY_LENGTH + 1, sizeof(char));
+  char *b32 = calloc(KEY_LENGTH + 1, sizeof(char));
 
-    // Initialize key buffer
-    memset(buffer, '\0', sizeof(char) * (KEY_LENGTH + 1));
-    memset(b32, '\0', sizeof(char) * (KEY_LENGTH + 1));
+  // Initialize key buffer
+  memset(buffer, '\0', sizeof(char) * (KEY_LENGTH + 1));
+  memset(b32, '\0', sizeof(char) * (KEY_LENGTH + 1));
 
-    // Generate random bytes for key
-    status = RAND_pseudo_bytes(buffer, KEY_LENGTH);
+  // Generate random bytes for key
+  status = RAND_pseudo_bytes(buffer, KEY_LENGTH);
 
-    // Warn users if bytes aren't sufficiently random
-    if (status != 1) {
-        fprintf(stderr, "%s\n", 
-            ERR_error_string( ERR_get_error(), 0 ));
+  // Warn users if bytes aren't sufficiently random
+  if (status != 1) {
+    fprintf(stderr, "%s\n", ERR_error_string(ERR_get_error(), 0));
+  }
+
+  // Convert each byte to a base32 char reperesentation
+  for (i = 0; i < KEY_LENGTH; i++) {
+    b32[i] = ((int)buffer[i] % 32);
+
+    // Offset value to appropriate ASCII char set
+    if ((int)b32[i] <= 5) {
+      b32[i] += 50;
+    } else {
+      b32[i] += 59;
     }
+  }
 
-    // Convert each byte to a base32 char reperesentation
-    for (i = 0; i < KEY_LENGTH; i++) {
-        b32[i] = ((int) buffer[i] % 32);
-        
-        // Offset value to appropriate ASCII char set
-        if ((int) b32[i] <= 5) { b32[i] += 50; }
-        else { b32[i] += 59; }
-    }
-
-    free(buffer);
-    return b32;
+  free(buffer);
+  return b32;
 }
 
-// Generate QR code for given uri and print to console
-// Based on demo code from @nayuki
-int _qr_img(const char* uri) {
-    int x, y, size, border;
-    uint8_t qr0[qrcodegen_BUFFER_LEN_MAX];
-    uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
-    bool ok = qrcodegen_encodeText(uri, tempBuffer,
-        qr0, qrcodegen_Ecc_LOW, qrcodegen_VERSION_MIN,
-        qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
-    if (!ok) {
-        fprintf(stderr, "Problem encoding uri to bytes\n");
-        return 1;
+// Based on
+// https://github.com/dengshiyong/a-example-about-libqd-and-libqrencode/blob/master/qrencode.c
+gdImagePtr _qr_img(QRcode *code, int fgColor[3], int bgColor[3], int size,
+                   int margin) {
+  int code_size = size / code->width;
+  if (code_size == 0) {
+    code_size = 1;
+  }
+  int img_width = code->width * code_size + 2 * margin;
+  gdImagePtr img = gdImageCreate(img_width, img_width);
+  int img_fgcolor =
+      gdImageColorAllocate(img, fgColor[0], fgColor[1], fgColor[2]);
+  int img_bgcolor =
+      gdImageColorAllocate(img, bgColor[0], bgColor[1], bgColor[2]);
+  gdImageFill(img, 0, 0, img_bgcolor);
+  u_char *p = code->data;
+  int x, y, posx, posy;
+
+  for (y = 0; y < code->width; y++) {
+    for (x = 0; x < code->width; x++) {
+      if (*p & 1) {
+        posx = x * code_size + margin;
+        posy = y * code_size + margin;
+        gdImageFilledRectangle(img, posx, posy, posx + code_size,
+                               posy + code_size, img_fgcolor);
+      }
+      p++;
     }
+  }
 
-    size = qrcodegen_getSize(qr0);
-	border = 2;
-	for (y = -border; y < size + border; y++) {
-		for (x = -border; x < size + border; x++) {
-			fputs((qrcodegen_getModule(qr0, x, y) ? "##" : "  "), stdout);
-		}
-		fputs("\n", stdout);
-	}
-	fputs("\n", stdout);
-
-    return 0;
+  return img;
 }
